d29608be228a23bd3d743d0559c6a073854485fa
Antonio Piccolo <antonio.piccolo@exelab.com>
2023-12-06 15:50:55 +0100

    Fix

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -123,10 +123,10 @@ async function handlePayment(req, res) {
     </div>`;
     await sendEmail(email, "CERAMICHINE - Gift Card", html);
     console.log("[CONTROLLER][HANDLE-PAYMENT] end");
-    return res.status(200).send({ message: "ok" });
+    return res.status(200).send();
   } catch (err) {
     console.error("[CONTROLLER][HANDLE-PAYMENT] error", err);
-    return res.status(500).send({ message: "error" });
+    return res.status(500).send();
   }
 }
 
